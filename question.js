9fe15cbd2024c86605b4fa134f17684bc86e9969
hamdi-91 <hamdidoss3@gmail.com>
2020-04-11 20:56:20 +0100

    quizapp1

---
+++ question.js
@@ -0,0 +1,44 @@
+var questions = [{
+    "question": " Javascript is ?",
+    "option1": "a server web",
+    "option2": "a scripting programming language ",
+    "option3": "web static page ",
+    "option4": "a devtools object",
+    "answer": "2",
+}, {
+    "question": "Inside which HTML element do we put the JavaScript??",
+    "option1": "<script>",
+    "option2": "<javascript>",
+    "option3": "<js>",
+    "option4": "<scripting>",
+    "answer": "1"
+}, {
+    "question": "What is the correct syntax for referring to an external script called 'xxx.js'?",
+    "option1": "<script href='xxx.js'>",
+    "option2": "<script name='xxx.js'>",
+    "option3": "<script src='xxx.js'>",
+    "option4": "<script file='xxx.js'>",
+    "answer": "3",
+}, {
+    "question": " How do you write 'Hello World' in an alert box?",
+    "option1": "msgBox('Hello World');",
+    "option2": "alertBox('Hello World');",
+    "option3": "msg('Hello World');",
+    "option4": "alert('Hello World');",
+    "answer": "4",
+}, {
+    "question": " Web pages are written using ?",
+    "option1": "FTP",
+    "option2": "HTTP",
+    "option3": "HTML",
+    "option4": "URL",
+    "answer": "3",
+}, {
+    "question": " C is  ?",
+    "option1": "A third generation high level language",
+    "option2": "A machine language",
+    "option3": "An assembly language",
+    "option4": "All of the above",
+    "answer": "1",
+}
+]
